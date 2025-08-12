601f0a601e16ddebe3988e28a316f6a76b0e2f31
Insu Hong <hinsu93@gmail.com>
2025-08-12 14:25:14 +0900

    #2.1 More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,23 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+export class CreateMovieDto {
+  readonly name: string;
+  readonly director: string;
+}
+
+export class UpdateMovieDto {
+  readonly name: string;
+  readonly director: string;
+}
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +26,31 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie for a movie made after: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') id: string) {
     return `This will return one movie with the id: ${id}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData: CreateMovieDto) {
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patchMovie(@Param('id') movieId: string) {
-    return `This will patch a movie with the id ${movieId}`;
+  @Patch(':id')
+  patchMovie(@Param('id') movieId: string, @Body() updateData: UpdateMovieDto) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
